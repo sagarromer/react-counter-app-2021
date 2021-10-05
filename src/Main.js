7d9bc4f0577e6d341d953ba6c62605980f917c3e
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-05 03:37:45 -0700

    increment decrement neutral logic implemented

--- src/Main.js
+++ src/Main.js
@@ -3,18 +3,31 @@ import React, { Component } from 'react'
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            number: 0
+        }
     }
+    increment=() =>{
+        this.setState({number : this.state.number+1});
+        console.log(this.state.number);
+        
 
+    }
+    decrement=() =>{
+        this.setState({number : this.state.number-1});
+    }
+    neutral=() =>{
+        this.setState({number : 0});
+    }
     render() {
         return (
             <div className='row justify-content-center'>
                 <div className='col-md-5' style={{marginTop:'250px', border:'5px solid black'}}>
                     <h1>React Counter</h1>
-                    <h1>0</h1>
-                    <button className='btn btn-success'>Increment</button>
-                    <button className='btn btn-danger'>Decrement</button>
-                    <button className='btn btn-primary'>Neutral</button>
+                    <h1>{this.state.number}</h1>
+                    <button className='btn btn-success' onClick={this.increment}>Increment</button>
+                    <button className='btn btn-danger' onClick={this.decrement}>Decrement</button>
+                    <button className='btn btn-primary' onClick={this.neutral}>Neutral</button>
                 </div>
             </div>
             
